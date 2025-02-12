d4dbd9cd80e6b9e51533dc3359d152d2daea2dba
Angel-Ru <angelrubio@paucasesnovescifp.cat>
2025-02-12 20:47:57 +0100

    Port 3000, les proves no han funcionat

--- API/server.js
+++ API/server.js
@@ -37,7 +37,7 @@ async function connectDB() {
     return poolPromise;
 }
 
-const port = process.env.PORT || 50000;
+const port = process.env.PORT || 3000;
 // Iniciar servidor
 app.listen(port, () => console.log('🚀 Servidor iniciat al port 3000'));
 
