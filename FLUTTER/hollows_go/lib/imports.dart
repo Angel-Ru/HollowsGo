0e45df5163a99c15e779eb11bb19fe737172a1c3
AlbertGalan <albertgalan@paucasesnovescifp.cat>
2025-02-17 14:13:30 +0100

    faves parades

--- FLUTTER/hollows_go/lib/imports.dart
+++ FLUTTER/hollows_go/lib/imports.dart
@@ -5,10 +5,11 @@ export '/screens/prehomescreen.dart';
 export '/screens/tendascreen.dart';
 export '/providers/loginprovider.dart';
 export '/providers/blinkingtextprovider.dart';
-export 'providers/ui_provider.dart';
+export '/providers/ui_provider.dart';
 
 export 'package:shared_preferences/shared_preferences.dart';
 export 'package:flutter/material.dart';
 export 'package:file_picker/file_picker.dart';
 export 'package:provider/provider.dart';
+
 export 'dart:math';
