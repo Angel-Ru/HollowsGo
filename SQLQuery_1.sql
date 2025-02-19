759a42661e7b943f5acc2e600459433a0d5a0456
MiquelSanso <miquelsanso@paucasesnovescifp.cat>
2025-02-19 23:53:42 +0100

    Create SQLQuery_1.sql

---
+++ SQLQuery_1.sql
@@ -0,0 +1,41 @@
+select * from PERSONATGES order by nom;
+
+select * from ENEMICS;
+
+select * from ATACS;
+
+/*
+insert into ENEMICS(personatge_id,punts_donats, musica_combat) VALUES (40,50, Null);
+insert into ENEMICS(personatge_id,punts_donats, musica_combat) VALUES (41,30, Null);
+insert into ENEMICS(personatge_id,punts_donats, musica_combat) VALUES (42,30, Null);
+insert into ENEMICS(personatge_id,punts_donats, musica_combat) VALUES (43,50, Null);
+insert into ENEMICS(personatge_id,punts_donats, musica_combat) VALUES (44,50, Null);
+insert into ENEMICS(personatge_id,punts_donats, musica_combat) VALUES (6,70, Null);
+insert into ENEMICS(personatge_id,punts_donats, musica_combat) VALUES (45,50, Null);
+insert into ENEMICS(personatge_id,punts_donats, musica_combat) VALUES (46,50, Null);
+insert into ENEMICS(personatge_id,punts_donats, musica_combat) VALUES (47,50, Null);
+insert into ENEMICS(personatge_id,punts_donats, musica_combat) VALUES (48,45, Null);
+insert into ENEMICS(personatge_id,punts_donats, musica_combat) VALUES (49,30, Null);
+insert into ENEMICS(personatge_id,punts_donats, musica_combat) VALUES (50,25, Null);
+insert into ENEMICS(personatge_id,punts_donats, musica_combat) VALUES (51,30, Null);
+insert into ENEMICS(personatge_id,punts_donats, musica_combat) VALUES (52,50, Null);
+insert into ENEMICS(personatge_id,punts_donats, musica_combat) VALUES (31,70, Null);
+insert into ENEMICS(personatge_id,punts_donats, musica_combat) VALUES (53,60, Null);
+insert into ENEMICS(personatge_id,punts_donats, musica_combat) VALUES (54,50, Null);
+insert into ENEMICS(personatge_id,punts_donats, musica_combat) VALUES (55,50, Null);
+insert into ENEMICS(personatge_id,punts_donats, musica_combat) VALUES (56,50, Null); */
+
+select * from skins;
+
+-- Categories: 1 estrella, 2 estrelles, 3 estrelles, 4 estrelles
+
+/*
+Aizens
+insert into SKINS(nom, categoria, imatge,personatge,atac) VALUES('Aizen Bo',2,'https://res.cloudinary.com/dkcgsfcky/image/upload/v1738748715/PERSONATGES/Aizen/vqulmklrsxme33zt2crk.png',1,1);
+insert into SKINS(nom, categoria, imatge,personatge,atac) VALUES('Aizen Dolent Ulleres',1,'https://res.cloudinary.com/dkcgsfcky/image/upload/v1738750831/ENEMICS/Aizen/u6xihhxt3zvaxqmhjbqo.png',1,1);
+insert into SKINS(nom, categoria, imatge,personatge,atac) VALUES('Aizen Dolent Arrancar',2,'https://res.cloudinary.com/dkcgsfcky/image/upload/v1738750833/ENEMICS/Aizen/jzh3wwccdfvzmdlgxy8z.png',1,1);
+insert into SKINS(nom, categoria, imatge,personatge,atac) VALUES('Aizen Dolent Xetat',3,'https://res.cloudinary.com/dkcgsfcky/image/upload/v1738750830/ENEMICS/Aizen/ir3c8h0q22fuvmvxjls0.png',1,1);
+insert into SKINS(nom, categoria, imatge,personatge,atac) VALUES('Aizen Dolent Garganta',2,'https://res.cloudinary.com/dkcgsfcky/image/upload/v1738750831/ENEMICS/Aizen/si6frlfn7ylwb2msmhro.png',1,1);
+insert into SKINS(nom, categoria, imatge,personatge,atac) VALUES('Aizen Dolent Hogyoku',4,'https://res.cloudinary.com/dkcgsfcky/image/upload/v1738750832/ENEMICS/Aizen/iqjkegmuz2dxkc3q0d0k.png',1,1);
+*/
+
