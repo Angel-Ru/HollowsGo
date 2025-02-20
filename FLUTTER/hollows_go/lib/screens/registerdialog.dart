fcfd47847961d791c037d6f03dfa4fd3b3bdb213
Angel-Ru <angelrubio@paucasesnovescifp.cat>
2025-02-20 17:39:23 +0100

    Registre i login funcional amb shared preferences activades, falta revisar que a pic surt un 1ms la pantalla de login pero entra a nes HomeScreen

--- FLUTTER/hollows_go/lib/screens/registerdialog.dart
+++ FLUTTER/hollows_go/lib/screens/registerdialog.dart
@@ -26,64 +26,65 @@ class _RegisterDialogState extends State<RegisterDialog> {
   }
 
   Future<void> _register() async {
-    final username = _usernameController.text.trim();
-    final email = _emailController.text.trim();
-    final password = _passwordController.text.trim();
+  final username = _usernameController.text.trim();
+  final email = _emailController.text.trim();
+  final password = _passwordController.text.trim();
 
-    if (username.isEmpty || email.isEmpty || password.isEmpty) {
-      ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(content: Text("Tots els camps son obligatoris")),
-      );
-      return;
-    }
-
-    setState(() {
-      _isLoading = true;
-    });
-
-    final url = Uri.parse('http://192.168.228.168:3000/usuaris/');
-    final headers = {'Content-Type': 'application/json'};
-    final body = jsonEncode({
-      "nom": username,
-      "email": email,
-      "contrassenya": password,
-    });
-
-    try {
-      final response = await http.post(url, headers: headers, body: body);
-
-      if (response.statusCode == 201) {
-        final prefs = await SharedPreferences.getInstance();
-        await prefs.setBool('isRegistered', true);
-
-        ScaffoldMessenger.of(context).showSnackBar(
-          SnackBar(content: Text("Te has registrat exitosament")),
-        );
-
-        Navigator.of(context).pop();
-        showDialog(
-          context: context,
-          builder: (context) => LoginScreen(),
-        );
-      } else {
-        final errorMsg = jsonDecode(response.body)['message'] ?? "Error desconegut";
-        ScaffoldMessenger.of(context).showSnackBar(
-          SnackBar(content: Text("Error: $errorMsg")),
-          //println de error
-          
-        );
-        print("Error: $errorMsg");
-      }
-    } catch (e) {
-      ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(content: Text("Error de connexió: $e")),
-      );
-    } finally {
-      setState(() {
-        _isLoading = false;
-      });
-    }
+  if (username.isEmpty || email.isEmpty || password.isEmpty) {
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(content: Text("Tots els camps són obligatoris")),
+    );
+    return;
   }
+
+  setState(() {
+    _isLoading = true;
+  });
+
+  final url = Uri.parse('http://192.168.2.197:3000/usuaris/');
+  final headers = {'Content-Type': 'application/json'};
+  final body = jsonEncode({
+    "nom": username,
+    "email": email,
+    "contrassenya": password,
+  });
+
+  try {
+    final response = await http.post(url, headers: headers, body: body);
+    final responseData = jsonDecode(response.body);
+
+    if (response.statusCode == 201) {
+      final user = responseData['user'];
+
+      final prefs = await SharedPreferences.getInstance();
+      await prefs.setBool('isRegistered', true);
+      await prefs.setBool('isLoggedIn', true); 
+      await prefs.setString('userEmail', user['email']);
+
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text("Te has registrado exitosamente")),
+      );
+
+      Navigator.of(context).pushReplacement(
+        MaterialPageRoute(builder: (context) => HomeScreen()),
+      );
+    } else {
+      final errorMsg = responseData['message'] ?? "Error desconocido";
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text("Error: $errorMsg")),
+      );
+    }
+  } catch (e) {
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(content: Text("Error de conexión: $e")),
+    );
+  } finally {
+    setState(() {
+      _isLoading = false;
+    });
+  }
+}
+
 
   @override
   Widget build(BuildContext context) {
@@ -91,63 +92,55 @@ class _RegisterDialogState extends State<RegisterDialog> {
       title: Text('Registra\'t'),
       content: Container(
         width: 300,
-        height: 200,
+        height: 250,
         child: Column(
           mainAxisSize: MainAxisSize.min,
           children: [
             const SizedBox(height: 32),
-            Expanded(
-              child: SingleChildScrollView(
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    TextField(
-                      controller: _usernameController,
-                      decoration: InputDecoration(
-                        labelText: 'Nom',
-                        prefixIcon: const Icon(Icons.person_outline),
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(8),
-                        ),
-                      ),
-                    ), const SizedBox(height: 16),
-                    TextField(
-                      controller: _emailController,
-                      decoration: InputDecoration(
-                        labelText: 'Email',
-                        prefixIcon: const Icon(Icons.person_outline),
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(8),
-                        ),
-                      ),
-                    ),
-                    const SizedBox(height: 16),
-                    TextField(
-                      controller: _passwordController,
-                      decoration: InputDecoration(
-                        labelText: 'Contrasenya',
-                        prefixIcon: const Icon(Icons.lock_outline),
-                        suffixIcon: IconButton(
-                          icon: Icon(
-                            _isPasswordVisible
-                                ? Icons.visibility_off
-                                : Icons.visibility,
-                          ),
-                          onPressed: () {
-                            setState(() {
-                              _isPasswordVisible = !_isPasswordVisible;
-                            });
-                          },
-                        ),
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(8),
-                        ),
-                      ),
-                      obscureText: !_isPasswordVisible,
-                    ),
-                  ],
+            TextField(
+              controller: _usernameController,
+              decoration: InputDecoration(
+                labelText: 'Nom',
+                prefixIcon: const Icon(Icons.person_outline),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(8),
                 ),
               ),
+            ),
+            const SizedBox(height: 16),
+            TextField(
+              controller: _emailController,
+              decoration: InputDecoration(
+                labelText: 'Email',
+                prefixIcon: const Icon(Icons.email_outlined),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(8),
+                ),
+              ),
+            ),
+            const SizedBox(height: 16),
+            TextField(
+              controller: _passwordController,
+              decoration: InputDecoration(
+                labelText: 'Contrasenya',
+                prefixIcon: const Icon(Icons.lock_outline),
+                suffixIcon: IconButton(
+                  icon: Icon(
+                    _isPasswordVisible
+                        ? Icons.visibility_off
+                        : Icons.visibility,
+                  ),
+                  onPressed: () {
+                    setState(() {
+                      _isPasswordVisible = !_isPasswordVisible;
+                    });
+                  },
+                ),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(8),
+                ),
+              ),
+              obscureText: !_isPasswordVisible,
             ),
           ],
         ),
