a42bc743bc9207a48de6b24a1e6d95fbd8125d6e
AlbertGalan <albertgalan@paucasesnovescifp.cat>
2025-02-21 09:05:14 +0100

    penne al pesto

--- FLUTTER/hollows_go/lib/screens/combatscreen.dart
+++ FLUTTER/hollows_go/lib/screens/combatscreen.dart
@@ -1,3 +1,4 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
 
 class CombatScreen extends StatefulWidget {
@@ -6,55 +7,59 @@ class CombatScreen extends StatefulWidget {
 }
 
 class _CombatScreenState extends State<CombatScreen> {
-  double ichigoHealth = 1.0; // Salud actual de Ichigo (valor entre 0 y 1)
-  double aizenHealth = 1.0; // Salud actual de Aizen (valor entre 0 y 1)
+  double aliatHealth = 1.0; // Salud actual de Ichigo (valor entre 0 y 1)
+  double enemicHealth = 1.0; // Salud actual de Aizen (valor entre 0 y 1)
 
-  // Vida máxima de los personajes (no cambia)
-  int ichigoHP = 1000; // Vida máxima de Ichigo
-  int aizenHP = 1000; // Vida máxima de Aizen
+  int aliatHPMX = 1000; // Vida máxima de Ichigo
+  int enemicHP = 1000; // Vida máxima de Aizen
+
+  late String backgroundImage;
+
+  @override
+  void initState() {
+    super.initState();
+    _setRandomBackground();
+  }
+
+  void _setRandomBackground() {
+    final random = Random();
+    int randomIndex = random.nextInt(4) + 1;
+    backgroundImage = 'lib/images/combat_proves/fondo_combat_$randomIndex.png';
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Stack(
         children: [
-          // Imagen de fondo
           Positioned.fill(
             child: Image.asset(
-              'lib/images/tenda_urahara.jpg',
+              backgroundImage,
               fit: BoxFit.cover,
             ),
           ),
-
-          // Contenido principal
           Padding(
             padding: const EdgeInsets.all(16.0),
             child: Column(
               children: [
-                // Parte superior: Imagen de Aizen y su barra de salud
                 Column(
-                  crossAxisAlignment:
-                      CrossAxisAlignment.center, // Centrar los elementos
+                  crossAxisAlignment: CrossAxisAlignment.center,
                   children: [
-                    // Imagen de Aizen, en la parte superior derecha
                     Image.asset(
                       'lib/images/combat_proves/aizen_combat.png',
-                      height: 300, // Imagen más grande del enemigo
+                      height: 300,
                       width: 300,
                     ),
                     SizedBox(height: 8),
-                    // Contenedor para la barra de vida de Aizen y su nombre
                     Container(
                       padding: EdgeInsets.all(10),
                       decoration: BoxDecoration(
-                        color: Colors.grey.shade300, // Gris claro
+                        color: Colors.grey.shade300,
                         borderRadius: BorderRadius.circular(10),
                       ),
                       child: Row(
-                        mainAxisAlignment: MainAxisAlignment
-                            .end, // Alineamos todo a la derecha
+                        mainAxisAlignment: MainAxisAlignment.end,
                         children: [
-                          // Nombre de Aizen (A la izquierda)
                           Flexible(
                             child: Text(
                               "Dondodochakka",
@@ -67,48 +72,35 @@ class _CombatScreenState extends State<CombatScreen> {
                               softWrap: true,
                             ),
                           ),
-                          SizedBox(
-                              width:
-                                  10), // Espacio entre el nombre y la barra de vida
-                          // Barra de vida de Aizen (Se acorta hacia la derecha)
-                          _buildHealthBar(aizenHealth, aizenHP),
+                          SizedBox(width: 10),
+                          _buildHealthBar(enemicHealth, enemicHP),
                         ],
                       ),
                     ),
                   ],
                 ),
-
-                // Espacio entre las secciones
                 Spacer(),
-
-                // Parte inferior: Imagen de Ichigo y su barra de salud
                 Column(
-                  crossAxisAlignment:
-                      CrossAxisAlignment.center, // Centrar los elementos
+                  crossAxisAlignment: CrossAxisAlignment.center,
                   children: [
-                    // Imagen de Ichigo, en la parte inferior izquierda
                     Image.asset(
                       'lib/images/combat_proves/bleach_combat.png',
-                      height: 250, // Imagen más grande del aliado
+                      height: 250,
                       width: 250,
                     ),
                     SizedBox(height: 8),
-                    // Contenedor gris para la barra de vida de Ichigo y su nombre + botón
                     Container(
                       padding: EdgeInsets.all(10),
                       decoration: BoxDecoration(
-                        color: Colors.grey.shade300, // Gris claro
+                        color: Colors.grey.shade300,
                         borderRadius: BorderRadius.circular(10),
                       ),
                       child: Column(
                         children: [
                           Row(
                             children: [
-                              // Barra de vida de Ichigo (Se acorta hacia la izquierda)
-                              _buildHealthBar(ichigoHealth, ichigoHP),
-                              SizedBox(
-                                  width:
-                                      10), // Espacio entre la barra de vida y el nombre
+                              _buildHealthBar(aliatHealth, aliatHPMX),
+                              SizedBox(width: 10),
                               Flexible(
                                 child: Text(
                                   "Ichigo",
@@ -123,19 +115,14 @@ class _CombatScreenState extends State<CombatScreen> {
                               ),
                             ],
                           ),
-                          // Botón de "Luchar" debajo de la barra de vida de Ichigo
                           SizedBox(height: 20),
                           ElevatedButton(
                             onPressed: () {
                               setState(() {
-                                // Solo Reducir vida de Aizen, sin modificar la vida de Ichigo
-                                if (aizenHealth > 0) {
-                                  // Restamos 100 puntos de vida de Aizen, sin dividir entre la vida máxima
-                                  aizenHealth -= 100 /
-                                      aizenHP; // Convertimos los 100 puntos a porcentaje.
-                                  if (aizenHealth < 0) {
-                                    aizenHealth =
-                                        0; // Aseguramos que no baje de 0
+                                if (enemicHealth > 0) {
+                                  enemicHealth -= 100 / enemicHP;
+                                  if (enemicHealth < 0) {
+                                    enemicHealth = 0;
                                   }
                                 }
                               });
@@ -143,11 +130,24 @@ class _CombatScreenState extends State<CombatScreen> {
                             style: ElevatedButton.styleFrom(
                               backgroundColor: Colors.orange,
                               padding: EdgeInsets.symmetric(
-                                  horizontal: 100, vertical: 15),
+                                  horizontal: 80, vertical: 12),
                               textStyle: TextStyle(
-                                  fontSize: 24, fontWeight: FontWeight.bold),
+                                fontSize: 18,
+                                fontWeight: FontWeight.bold,
+                              ),
                             ),
-                            child: Text("LUCHAR"),
+                            child: Column(
+                              children: [
+                                Text("LUCHAR", style: TextStyle(fontSize: 18)),
+                                SizedBox(height: 4),
+                                Text(
+                                  "Bankai Getsuga Tensho",
+                                  style: TextStyle(
+                                      fontSize: 12,
+                                      fontWeight: FontWeight.w500),
+                                ),
+                              ],
+                            ),
                           ),
                         ],
                       ),
@@ -162,24 +162,17 @@ class _CombatScreenState extends State<CombatScreen> {
     );
   }
 
-  // Barra de salud con animación y colores según la salud
   Widget _buildHealthBar(double health, int hp) {
-    // Determinamos el color de la barra de vida
     Color barColor = Colors.green;
-    if (health < 0.6) {
-      barColor = Colors.orange;
-    }
-    if (health < 0.2) {
-      barColor = Colors.red;
-    }
+    if (health < 0.6) barColor = Colors.orange;
+    if (health < 0.2) barColor = Colors.red;
 
     return Stack(
       alignment: Alignment.center,
       children: [
-        // Fondo de la barra de salud
         Positioned(
           child: Container(
-            width: 200, // Acortamos la barra a 200
+            width: 200,
             height: 24,
             decoration: BoxDecoration(
               color: Colors.grey.shade700,
@@ -187,8 +180,6 @@ class _CombatScreenState extends State<CombatScreen> {
             ),
           ),
         ),
-
-        // Barra de salud con animación
         Positioned(
           left: 0,
           child: ClipRRect(
@@ -198,7 +189,7 @@ class _CombatScreenState extends State<CombatScreen> {
               duration: Duration(milliseconds: 500),
               builder: (context, value, child) {
                 return Container(
-                  width: 200 * value, // Acortamos la barra a 200
+                  width: 200 * value,
                   height: 24,
                   decoration: BoxDecoration(
                     color: barColor,
@@ -209,11 +200,9 @@ class _CombatScreenState extends State<CombatScreen> {
             ),
           ),
         ),
-
-        // Borde negro encima de la barra
         Positioned(
           child: Container(
-            width: 200, // Acortamos la barra a 200
+            width: 200,
             height: 24,
             decoration: BoxDecoration(
               borderRadius: BorderRadius.circular(10),
@@ -221,11 +210,9 @@ class _CombatScreenState extends State<CombatScreen> {
             ),
           ),
         ),
-
-        // Número de salud
         Positioned(
           child: Text(
-            "${(health * hp).toInt()}/$hp", // Mostrar la vida actual sobre la máxima
+            "${(health * hp).toInt()}/$hp",
             style: TextStyle(
               color: Colors.white,
               fontSize: 16,
