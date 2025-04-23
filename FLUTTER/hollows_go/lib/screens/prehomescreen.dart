b3d0cac6ffc66446ee0b31040d932451cab220c1
MiquelSanso <miquelsanso@paucasesnovescifp.cat>
2025-04-23 17:43:07 +0200

    llevades imatges locals + modificacions a la prehomescreen

--- FLUTTER/hollows_go/lib/screens/prehomescreen.dart
+++ FLUTTER/hollows_go/lib/screens/prehomescreen.dart
@@ -13,19 +13,8 @@ class PreHomeScreen extends StatefulWidget {
 
 class _PreHomeScreenState extends State<PreHomeScreen>
     with SingleTickerProviderStateMixin {
-  // IMAGES OF KON FOR THE RANDOM IMAGE ARRAY
-  final List<String> imagePaths = [
-    'lib/images/prehomescreen_imatges/koncapitan.png',
-    'lib/images/prehomescreen_imatges/konepico.png',
-    'lib/images/prehomescreen_imatges/konlike.png',
-    'lib/images/prehomescreen_imatges/konbrillitos.png',
-    'lib/images/prehomescreen_imatges/konrap.png',
-  ];
-
   late AnimationController _controller;
   late Animation<double> _opacityAnimation;
-  late ScrollController _scrollControllerTop;
-  late ScrollController _scrollControllerBottom;
   // late AudioPlayer _audioPlayer;
 
   // INICIALATE THE ANIMATION CONTROLLER AND THE SCROLL CONTROLLERS
@@ -47,12 +36,6 @@ class _PreHomeScreenState extends State<PreHomeScreen>
 
     _controller.repeat(reverse: true);
 
-    _scrollControllerTop = ScrollController();
-    _scrollControllerBottom = ScrollController();
-
-    _startAutoScroll(_scrollControllerTop);
-    _startAutoScroll(_scrollControllerBottom);
-
     //_audioPlayer = AudioPlayer();
     //_playBackgroundMusic();
   }
@@ -66,42 +49,16 @@ class _PreHomeScreenState extends State<PreHomeScreen>
   }*/
 
   // THIS FUNCTION IS FOR THE SCROLLING ANIMATION OF THE SKULLS + LOOPING
-  void _startAutoScroll(ScrollController scrollController) {
-    Future.doWhile(() async {
-      await Future.delayed(Duration(milliseconds: 20));
-      if (scrollController.hasClients) {
-        double maxScroll = scrollController.position.maxScrollExtent;
-        double currentScroll = scrollController.position.pixels;
-
-        if (currentScroll >= maxScroll) {
-          scrollController.jumpTo(0);
-        } else {
-          scrollController.animateTo(
-            currentScroll + 15,
-            duration: Duration(milliseconds: 100),
-            curve: Curves.linear,
-          );
-        }
-      }
-      return true;
-    });
-  }
 
   @override
   void dispose() {
     _controller.dispose();
-    _scrollControllerTop.dispose();
-    _scrollControllerBottom.dispose();
     //_audioPlayer.dispose();
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    // KON RANDOM IMAGE
-    final random = Random();
-    final randomImage = random.nextInt(imagePaths.length);
-
     return Scaffold(
       backgroundColor: Color(0xFFEAE4F2),
       resizeToAvoidBottomInset: false,
@@ -112,80 +69,39 @@ class _PreHomeScreenState extends State<PreHomeScreen>
         },
         child: Stack(
           children: [
-            Column(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Container(
-                  height: 40,
-                  child: ListView.builder(
-                    controller: _scrollControllerTop,
-                    scrollDirection: Axis.horizontal,
-                    itemCount: 100,
-                    itemBuilder: (context, index) {
-                      return Container(
-                        width: 39,
-                        height: 39,
-                        child: Image.asset(
-                          // CHANGE THE IMAGE + UPLOAD IT TO CLOUDINARY
-                          "lib/images/prehomescreen_imatges/skull_border.png",
-                          fit: BoxFit.cover,
-                        ),
-                      );
-                    },
-                  ),
-                ),
-                Spacer(),
-                Container(
-                  height: 200,
-                  child: Image.asset(
-                    imagePaths[randomImage],
-                    fit: BoxFit.cover,
-                  ),
-                ),
-                SizedBox(height: 70),
-                AnimatedBuilder(
-                  animation: _opacityAnimation,
-                  builder: (context, child) {
-                    return Opacity(
-                      opacity: _opacityAnimation.value,
-                      child: Text(
-                        "CLICA A ON SIGUI PER COMENÇAR",
-                        style: TextStyle(
-                          fontSize: 16,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                    );
-                  },
-                ),
-                SizedBox(height: 150),
-                Container(
-                  height: 40,
-                  child: ListView.builder(
-                    controller: _scrollControllerBottom,
-                    scrollDirection: Axis.horizontal,
-                    itemCount: 100,
-                    itemBuilder: (context, index) {
-                      return Container(
-                        width: 39,
-                        height: 39,
-                        child: Image.asset(
-                          "lib/images/prehomescreen_imatges/skull_border.png",
-                          fit: BoxFit.cover,
-                        ),
-                      );
-                    },
-                  ),
-                ),
-              ],
+            Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Spacer(),
+                  SizedBox(height: 70),
+                  if (_opacityAnimation != null)
+                    AnimatedBuilder(
+                      animation: _opacityAnimation,
+                      builder: (context, child) {
+                        return Opacity(
+                          opacity: _opacityAnimation.value,
+                          child: Text(
+                            "CLICA A ON SIGUI PER COMENÇAR",
+                            style: TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        );
+                      },
+                    ),
+                  SizedBox(height: 150),
+                ],
+              ),
             ),
             Center(
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.start,
                 children: [
                   SizedBox(height: 150),
-                  Image.asset(
-                    "lib/images/prehomescreen_imatges/nom_aplicacio.png",
+                  Image.network(
+                    "https://res.cloudinary.com/dkcgsfcky/image/upload/v1744708246/PREHOMESCREEN/ee9hwiaahvn6mj2dcnov.png",
                     width: 300,
                   ),
                 ],
