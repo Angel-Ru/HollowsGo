aee251cbdb6b88cca04bba25e0a6eea44ea2a805
Angel-Ru <angelrubio@paucasesnovescifp.cat>
2025-05-18 13:41:20 +0200

    Correcció de la ruta per agafar l'id de l'usuari

--- API/routes/equipamentRoutes.js
+++ API/routes/equipamentRoutes.js
@@ -5,7 +5,7 @@ const verificacioUsuari = require('../middlewares/verificacioUsuari');
 
 
 // Endpoint per obtenir les armes predefinides d’una skin
-router.get('/skins/:skin_id/armes', verificacioUsuari.verifyToken, equipamentController.getArmesPredefinidesPerSkin);
+router.get('/skins/:skin_id/armes/:usuari_id', verificacioUsuari.verifyToken, equipamentController.getArmesPredefinidesPerSkin);
 
 // Endpoint per equipar una arma
 router.post('/equipament', verificacioUsuari.verifyToken, equipamentController.equiparArmaASkin);
