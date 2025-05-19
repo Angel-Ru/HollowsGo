63b5a4fcde00b89811f9ad57018534b25be2fa33
Angel-Ru <angelrubio@paucasesnovescifp.cat>
2025-05-19 10:31:30 +0200

    Exportació correcte de les rutes del combat

--- API/routes/combatRoutes.js
+++ API/routes/combatRoutes.js
@@ -4,3 +4,5 @@ const combatController = require('../controllers/combatController');
 const verificacioUsuari = require('../middlewares/verificacioUsuari');
 
 router.post('/combatvida', verificacioUsuari.verifyToken, combatController.updateVidaActualSkin);
+
+module.exports = router;
