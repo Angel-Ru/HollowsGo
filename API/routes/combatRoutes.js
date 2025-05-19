7ff72b85a138a68d15659ea21c2f28b635f9c488
Angel-Ru <angelrubio@paucasesnovescifp.cat>
2025-05-19 10:51:08 +0200

    Correcció del parametre una altre pic

--- API/routes/combatRoutes.js
+++ API/routes/combatRoutes.js
@@ -3,6 +3,6 @@ const router = express.Router();
 const combatController = require('../controllers/combatController');
 const verificacioUsuari = require('../middlewares/verificacioUsuari');
 
-router.post('/vida/:skin_id', verificacioUsuari.verifyToken, combatController.updateVidaActualSkin);
+router.post('/vida/:id', verificacioUsuari.verifyToken, combatController.updateVidaActualSkin);
 
 module.exports = router;
