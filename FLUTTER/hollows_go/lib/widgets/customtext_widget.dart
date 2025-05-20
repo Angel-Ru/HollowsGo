1b1d8fe1479d9975e511287d153648048acfc2db
Angel-Ru <angelrubio@paucasesnovescifp.cat>
2025-05-20 12:37:45 +0200

    Dialegs amb animació i creació dels formulari de texte per login o registre pasat a un widget

---
+++ FLUTTER/hollows_go/lib/widgets/customtext_widget.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+class CustomTextField extends StatelessWidget {
+  final TextEditingController controller;
+  final String label;
+  final IconData icon;
+  final bool obscureText;
+  final Widget? suffixIcon;
+
+  const CustomTextField({
+    Key? key,
+    required this.controller,
+    required this.label,
+    required this.icon,
+    this.obscureText = false,
+    this.suffixIcon,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      controller: controller,
+      obscureText: obscureText,
+      style: const TextStyle(color: Colors.white),
+      decoration: InputDecoration(
+        labelText: label,
+        labelStyle: const TextStyle(color: Color.fromARGB(233, 255, 255, 255)),
+        prefixIcon: Icon(icon, color: Color.fromARGB(233, 255, 255, 255)),
+        suffixIcon: suffixIcon,
+        filled: true,
+        fillColor: Colors.white.withOpacity(0.1),
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(12),
+          borderSide: BorderSide.none,
+        ),
+      ),
+    );
+  }
+}
