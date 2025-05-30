4a4f1606f885e3212a323f54dd186d7b2bec81f8
Albert Galan <159948647+AlbertGalan@users.noreply.github.com>
2025-05-30 11:45:55 +0200

    afegida logica audio de prehomescreen a audioservice

--- FLUTTER/hollows_go/lib/screens/prehomescreen.dart
+++ FLUTTER/hollows_go/lib/screens/prehomescreen.dart
@@ -8,18 +8,7 @@ class PreHomeScreen extends StatefulWidget {
 
 class _PreHomeScreenState extends State<PreHomeScreen>
     with SingleTickerProviderStateMixin, WidgetsBindingObserver {
-  final List<String> imagePaths = [
-    'lib/images/imatges_prehomescreen/0c7569c5931f07a4fbce4e1dd58f9684.jpg',
-    'lib/images/imatges_prehomescreen/28bee056b92ef5af41ab8d7cc6f6949a.jpg',
-    'lib/images/imatges_prehomescreen/47a6c1657d9d7a35d1e852d28cd2316c.jpg',
-    'lib/images/imatges_prehomescreen/69c392ced882d589286c840dbfcac9cb.jpg',
-    'lib/images/imatges_prehomescreen/73fa9d142edb2cde3417eecc0d15bfcb.jpg',
-    'lib/images/imatges_prehomescreen/5951c02f2a2a74dea733a108549756f2.jpg',
-    'lib/images/imatges_prehomescreen/a82b5790310afc901065789e507b584a.jpg',
-    'lib/images/imatges_prehomescreen/a571d7d77159e05c9c35eaf4032c546b.jpg',
-    'lib/images/imatges_prehomescreen/c7fbad03a224d0ee498ff8194ba14314.jpg',
-    'lib/images/imatges_prehomescreen/c7285af40c27b635bab67680262bcd84.jpg',
-  ];
+  final List<String> imagePaths = [/* ... */];
 
   late String randomBackground;
   late AnimationController _controller;
@@ -30,7 +19,6 @@ class _PreHomeScreenState extends State<PreHomeScreen>
   @override
   void initState() {
     super.initState();
-
     WidgetsBinding.instance.addObserver(this);
 
     final random = Random();
@@ -56,9 +44,9 @@ class _PreHomeScreenState extends State<PreHomeScreen>
     _startAutoScroll(_scrollControllerTop);
     _startAutoScroll(_scrollControllerBottom);
 
-    AudioService.instance.play(
-      'https://res.cloudinary.com/dkcgsfcky/video/upload/v1745996030/MUSICA/fkgjkz7ttdqxqakacqsd.mp3',
-    );
+    /// 🔊 Nova crida unificada:
+    AudioService.instance.playScreenMusic('prehome');
+
     Provider.of<DialogueProvider>(context, listen: false)
         .loadDialogueFromJson("ichigo");
   }
@@ -71,7 +59,7 @@ class _PreHomeScreenState extends State<PreHomeScreen>
     _scrollControllerTop.dispose();
     _scrollControllerBottom.dispose();
 
-    AudioService.instance.stop(); // Per si de cas l'usuari surt abans
+    AudioService.instance.stop();
 
     super.dispose();
   }
@@ -79,12 +67,9 @@ class _PreHomeScreenState extends State<PreHomeScreen>
   @override
   void didChangeAppLifecycleState(AppLifecycleState state) {
     super.didChangeAppLifecycleState(state);
-
     if (state == AppLifecycleState.paused) {
-      // App va a background, pausa la música
       AudioService.instance.pause();
     } else if (state == AppLifecycleState.resumed) {
-      // App torna a foreground, reprèn la música
       AudioService.instance.resume();
     }
   }
@@ -95,7 +80,6 @@ class _PreHomeScreenState extends State<PreHomeScreen>
       if (scrollController.hasClients) {
         double maxScroll = scrollController.position.maxScrollExtent;
         double currentScroll = scrollController.position.pixels;
-
         if (currentScroll >= maxScroll) {
           scrollController.jumpTo(0);
         } else {
@@ -145,9 +129,7 @@ class _PreHomeScreenState extends State<PreHomeScreen>
               ),
             ),
             Positioned.fill(
-              child: Container(
-                color: Colors.black.withOpacity(0.4),
-              ),
+              child: Container(color: Colors.black.withOpacity(0.4)),
             ),
             Center(
               child: Column(
